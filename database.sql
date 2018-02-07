15f1d570dc0b229ebb937209300a5175f89cac8f
Neha <gaikwad.n@husky.neu.edu>
2018-02-07 00:20:53 -0500

    Uploading Files

---
+++ database.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Community Edition- MySQL GUI v7.01 
+MySQL - 5.0.27-community-nt : Database - multilevelsecurity
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`multilevelsecurity` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `multilevelsecurity`;
+
+/*Table structure for table `user_details` */
+
+DROP TABLE IF EXISTS `user_details`;
+
+CREATE TABLE `user_details` (
+  `Fullname` varchar(100) default NULL,
+  `Address` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `mobile` varchar(100) default NULL,
+  `DOB` varchar(100) default NULL,
+  `Gender` varchar(100) default NULL,
+  `username` varchar(100) default NULL,
+  `password` varchar(100) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_details` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
